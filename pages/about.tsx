e2e11bc33c0b189c78f7a22bd5a8bf5147fc0c0c
daggron <abhay.sharma@smallcase.com>
2021-09-21 10:29:35 +0530

    fix: max width for better user expeience and fixed seo of about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import SeoManager from '../components/SeoManager/SeoManager';
+import metaData from '../data/metaData';
 import About from '../scenes/About/About';
 
 export default function AboutPage() {
-  return <About />;
+  return (
+    <>
+      <SeoManager
+        pageTitle={'Bad Code | About me'}
+        title={metaData.title}
+        description={metaData.description}
+        tags={metaData.tags}
+        slug="/about"
+      />
+      <About />
+    </>
+  );
 }
