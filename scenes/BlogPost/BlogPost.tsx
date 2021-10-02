26a3198039fc23e326f2699651f2ef7cc56c3121
daggron <abhay.sharma@smallcase.com>
2021-10-02 22:17:13 +0530

    fix: fixed typography in the blog and added support for embedding codepens and other tools

--- scenes/BlogPost/BlogPost.tsx
+++ scenes/BlogPost/BlogPost.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { MDXRemote } from 'next-mdx-remote';
+import Image from 'next/image';
 
 import { H1, H2, H3, H4, H5, H6 } from '../../components/Typography/Heading';
 import { LinkComponent } from '../../components/Typography';
+import { CodeSandbox, CodePen } from 'mdx-embed';
 
 const components = {
   H1,
@@ -12,7 +14,10 @@ const components = {
   H5,
   H6,
   LinkComponent,
-}
+  Image,
+  CodeSandbox,
+  CodePen,
+};
 
 function BlogPost(props) {
   return (
