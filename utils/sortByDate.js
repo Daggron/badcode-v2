50e1603342eee93be0fc8b51307e59ba25cdebc1
daggron <abhay.sharma@smallcase.com>
2021-10-16 10:54:04 +0530

    fix: updated date sorting algo andf fixed logic flaws

--- utils/sortByDate.js
+++ utils/sortByDate.js
@@ -1,11 +1,36 @@
 const sortByDate = (data) => [...data].sort((a, b) => {
-  if (
-    new Date(a.data.date).getFullYear() < new Date(b.data.date).getFullYear()
-  ) {
+  const dateOne = new Date(a.data.date);
+  const dateTwo = new Date(b.data.date);
+
+  console.log(dateOne, dateTwo);
+
+  if (dateOne.getFullYear() > dateTwo.getFullYear()) {
     return 1;
   }
 
-  return -1;
+  if (dateOne.getFullYear() < dateTwo.getFullYear()) {
+    return -1;
+  }
+
+  if (dateOne.getFullYear() === dateTwo.getFullYear()) {
+    if (dateOne.getMonth() > dateTwo.getMonth()) {
+      return -1;
+    }
+
+    if (dateOne.getMonth() < dateTwo.getMonth()) {
+      return 1;
+    }
+
+    if (dateOne.getMonth() === dateTwo.getMonth()) {
+      if (dateOne.getDate() > dateTwo.getDate()) {
+        return -1;
+      }
+
+      return 1;
+    }
+  }
+
+  return 1;
 });
 
 export default sortByDate;
